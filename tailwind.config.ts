1a68c1d432f448efd4bf1ced694382481a772190
DJ Neill <dj.neill856@gmail.com>
2024-08-01 04:28:20 -0500

    set background for mobile, tablet, desktop

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "mobile-bg": "url('/images/background-mobile.svg')",
+        "tablet-bg": "url('/images/background-tablet.svg')",
+        "desktop-bg": "url('/images/background-desktop.svg')",
       },
     },
   },
