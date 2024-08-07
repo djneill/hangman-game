7e8ee7c4b7331b5bb668429d367cb9978fb530cd
DJ Neill <dj.neill856@gmail.com>
2024-08-06 23:23:42 -0500

    fix TW to accommodate laptop / desktop. How to page complete for all screens.

--- src/app/howToPlay/page.tsx
+++ src/app/howToPlay/page.tsx
@@ -11,31 +11,33 @@ interface Rule {
 
 export default function Page() {
     return (
-        <div className='min-h-screen w-[324px] sm:w-[680px] mx-auto mt-10'>
-            <div className="flex justify-between sm:flex-row mb-12 items-center">
+        <div className='min-h-screen w-[324px] sm:w-[680px] lg:w-[1024px] lg:max-w-[1216px] mx-auto mt-10'>
+            <div className="flex justify-between sm:flex-row mb-16 items-center">
                 <Link href={'/'}>
                     <BackButton />
                 </Link>
-                <div className='text-heading-m sm:text-heading-mid sm:flex sm:mx-auto sm:self-center gradient-text'
+                <div className='text-heading-m sm:text-heading-mid lg:text-heading-xl sm:flex sm:mx-auto sm:self-center gradient-text'
                     data-text="How to Play"
                 >
                     How to Play
                 </div>
             </div>
 
-            {rules.map((rule: Rule) => (
-                <div key={rule.id} className='rounded-xl p-6 mb-8 bg-white'>
-                    <div className="body mb-4">
-                        <div className='sm:flex sm:flex-row '>
-                            <span className='text-blue-600 mr-4 sm:mr-8 sm:self-center sm:text-heading-l'>{rule.id}</span>
-                            <span className='sm:w-full'>
-                                <span className='text-blue-800 sm:text-[40px] sm:tracking-wide'>{rule.title.toUpperCase()}</span>
-                                <p className='body-s text-gray-600'>{rule.description}</p>
-                            </span>
+            <div className='lg:grid lg:grid-cols-3 lg:gap-8 md:w-full'>
+                {rules.map((rule: Rule) => (
+                    <div key={rule.id} className='rounded-xl p-6 mb-8 bg-white'>
+                        <div className="body mb-4">
+                            <div className='sm:flex sm:flex-row lg:flex-col lg:text-center '>
+                                <span className='text-blue-600 mr-4 sm:mr-8 sm:self-center sm:text-heading-l lg:mb-8'>{rule.id}</span>
+                                <span className='sm:w-full'>
+                                    <span className='text-blue-800 sm:text-[40px] sm:tracking-wide'>{rule.title.toUpperCase()}</span>
+                                    <p className='body-s text-gray-600 lg:mt-8'>{rule.description}</p>
+                                </span>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
