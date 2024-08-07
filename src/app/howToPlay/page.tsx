d524cfafd0d966c580003b0f682ab34e1392150b
DJ Neill <dj.neill856@gmail.com>
2024-08-06 19:43:11 -0500

    adjust TW in .map() for rules to fit both mobile and tablet design.

--- src/app/howToPlay/page.tsx
+++ src/app/howToPlay/page.tsx
@@ -11,28 +11,43 @@ interface Rule {
 
 export default function Page() {
     return (
-        <div className='min-h-screen w-[324px] mx-auto mt-8'>
-            <div className="flex justify-between mb-10 items-center">
+        <div className='min-h-screen w-[324px] sm:w-[680px] mx-auto mt-10'>
+            <div className="flex justify-between sm:flex-row mb-12 items-center">
                 <Link href={'/'}>
                     <BackButton />
                 </Link>
-                <span className='heading-m sm:heading-xl gradient-text'
-                    data-text="How To Play"
+                <div className='text-heading-m sm:text-heading-mid sm:flex sm:mx-auto sm:self-center gradient-text'
+                    data-text="How to Play"
                 >
-                    How To Play
-                </span>
+                    How to Play
+                </div>
             </div>
 
+            {rules.map((rule: Rule) => (
+                <div key={rule.id} className='rounded-xl p-6 mb-8 bg-white'>
+                    <div className="body mb-4">
+                        <div className='sm:flex sm:flex-row '>
+                            <span className='text-blue-600 mr-4 sm:mr-8 sm:self-center sm:text-heading-l'>{rule.id}</span>
+                            <span className='sm:w-full'>
+                                <span className='text-blue-800 sm:text-[40px] sm:tracking-wide'>{rule.title.toUpperCase()}</span>
+                                <p className='body-s text-gray-600'>{rule.description}</p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+{/* *** reference note ***
             {rules.map((rule: Rule) => (
                 <div key={rule.id} className='rounded-xl p-6 mb-6 bg-white'>
                     <div className="body mb-2">
                         <span className='text-blue-600 mr-4'>{rule.id}</span>
                         <span className='text-blue-800'>{rule.title.toUpperCase()}</span>
+                        <p className='body-s text-gray-600'>{rule.description}</p>
                     </div>
-                    <p className='body-s text-gray-600'>{rule.description}</p>
-                </div>
-            ))}
 
-        </div>
-    )
-}
+                </div>
+            ))} */}
