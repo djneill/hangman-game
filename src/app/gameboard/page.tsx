3cc6be184ea1410ca9ffbeb481108a050a73cb9f
DJ Neill <dj.neill856@gmail.com>
2024-08-13 00:18:48 -0500

    work on render letter placeholders, split string by word so the entire word will flex-wrap if too long. Split by letter to build individual placeholders.

--- src/app/gameboard/page.tsx
+++ src/app/gameboard/page.tsx
@@ -48,15 +48,46 @@ export default function Gameboard() {
         });
     };
 
+    // const renderWordPlaceholders = () => {
+    //     if (!gameState) return null;
+    //     return gameState.word.split('').map((letter, index) => {
+    //         if (letter === ' ') {
+    //             return <div key={index} className='w-6 sm:w-8 lg:w-10'></div>;
+    //         }
+    //         return (
+    //             <div key={index}
+    //                 className='w-10 h-[66px] sm:w-[88px] sm:h-28 lg:w-28 lg:h-32 mx-2 shadow-category-btn rounded-2xl bg-light-blue flex justify-center items-center text-heading-m lg:text-heading-l'
+    //             >
+    //                 {gameState.revealedLetters.has(letter.toUpperCase()) ? letter : ''}
+    //             </div>
+    //         );
+    //     });
+    // };
     const renderWordPlaceholders = () => {
         if (!gameState) return null;
-        return gameState.word.split('').map((letter, index) => (
-            <div key={index}
-                className='w-10 h-[66px] sm:w-[88px] sm:h-28 lg:w-28 lg:h-32 mx-2 shadow-category-btn rounded-2xl bg-light-blue flex justify-center items-center text-heading-m lg:text-heading-l'
-            >
-                {gameState.revealedLetters.has(letter) ? letter : ''}
+        const words = gameState.word.split(' ');
+
+        return (
+            <div className="flex flex-wrap justify-center gap-4">
+                {words.map((word, wordIndex) => (
+                    <React.Fragment key={wordIndex}>
+                        <div className="flex">
+                            {word.split('').map((letter, letterIndex) => (
+                                <div
+                                    key={`${wordIndex}-${letterIndex}`}
+                                    className='w-10 h-[66px] sm:w-[88px] sm:h-28 lg:w-28 lg:h-32 mx-1 sm:mx-2 shadow-category-btn rounded-2xl bg-light-blue flex justify-center items-center text-heading-m lg:text-heading-l'
+                                >
+                                    {gameState.revealedLetters.has(letter.toUpperCase()) ? letter : ''}
+                                </div>
+                            ))}
+                        </div>
+                        {wordIndex < words.length - 1 && (
+                            <div className="w-6 sm:w-8 lg:w-10"></div>
+                        )}
+                    </React.Fragment>
+                ))}
             </div>
-        ));
+        );
     };
 
     const renderKeyboard = () => {
@@ -101,7 +132,7 @@ export default function Gameboard() {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 54 50" className='w-[26px] h-[24px] sm:w-[53px] sm:h-[48px] lg:w-[54px] lg:h-[50px] ml-8'><path fill="url(#a)" d="m26.667 49.467-3.867-3.52C9.067 33.493 0 25.253 0 15.2 0 6.96 6.453.533 14.667.533c4.64 0 9.093 2.16 12 5.547 2.906-3.387 7.36-5.547 12-5.547C46.88.533 53.333 6.96 53.333 15.2c0 10.053-9.066 18.293-22.8 30.747l-3.866 3.52Z" /><defs><linearGradient id="a" x1="26.667" x2="26.667" y1="8.567" y2="49.467" gradientUnits="userSpaceOnUse"><stop stop-color="#FE71FE" /><stop offset="1" stop-color="#7199FF" /></linearGradient></defs></svg>
                 </div>
             </div>
-            <div className="flex justify-center my-8">
+            <div className="flex justify-center items-center my-12">
                 {renderWordPlaceholders()}
             </div>
             <div className="grid grid-cols-9 gap-1 justify-center">
