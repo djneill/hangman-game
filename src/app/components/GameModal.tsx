a9e7916c5de979645287f0bedfdd9372662ebef9
DJ Neill <dj.neill856@gmail.com>
2024-08-19 13:50:12 -0500

    continue game modal for pause, win/lose. Position header & button layout.

--- src/app/components/GameModal.tsx
+++ src/app/components/GameModal.tsx
@@ -8,7 +8,7 @@ interface GameModalProps {
     onQuit: () => void;
 }
 
-const GameModal: React.FC<GameModalProps> = ({ type, onContinue, onNewCategory, onQuit }) => {
+export default function GameModal({ type, onContinue, onNewCategory, onQuit }: GameModalProps) {
     const titles = {
         paused: 'Paused',
         win: 'You Win',
@@ -17,33 +17,38 @@ const GameModal: React.FC<GameModalProps> = ({ type, onContinue, onNewCategory, 
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 px-4">
-            <div className="card w-full max-w-[324px] sm:max-w-[592px]">
-                <div className="flex flex-col items-center justify-between h-full py-8 px-4">
-                    <h2 className="text-heading-l sm:text-heading-xl text-white text-center mb-8">{titles[type]}</h2>
-                    <div className="flex flex-col space-y-4 w-full max-w-[260px]">
-                        <button
-                            onClick={onContinue}
-                            className="shadow-category-btn hover:shadow-category-btn-hover bg-[#2463FF] hover:bg-[#519dfb] text-white py-4 px-6 rounded-full w-full transition-all duration-300"
-                        >
-                            Continue
-                        </button>
-                        <button
-                            onClick={onNewCategory}
-                            className="shadow-category-btn hover:shadow-category-btn-hover bg-[#2463FF] hover:bg-[#519dfb] text-white py-4 px-6 rounded-full w-full transition-all duration-300"
-                        >
-                            New Category
-                        </button>
-                        <button
-                            onClick={onQuit}
-                            className="shadow-category-btn hover:shadow-category-btn-hover bg-[#FE71FE] hover:bg-[#7199FF] text-white py-4 px-6 rounded-full w-full transition-all duration-300"
-                        >
-                            Quit Game
-                        </button>
+            <div className="relative w-full max-w-[324px] sm:max-w-[592px]">
+                <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                    <div className="text-heading-l sm:text-heading-xl gradient-text"
+                        data-text={titles[type]}>
+                        {titles[type]}
+                    </div>
+                </div>
+                <div className="card w-full">
+                    <div className="flex flex-col items-center justify-between h-full py-16 px-4">
+                        <div className="flex flex-col space-y-4 w-full max-w-[260px] my-auto">
+                            <button
+                                onClick={onContinue}
+                                className="shadow-category-btn hover:shadow-category-btn-hover bg-[#2463FF] hover:bg-[#519dfb] text-white py-4 px-6 rounded-full w-full transition-all duration-300"
+                            >
+                                Continue
+                            </button>
+                            <button
+                                onClick={onNewCategory}
+                                className="shadow-category-btn hover:shadow-category-btn-hover bg-[#2463FF] hover:bg-[#519dfb] text-white py-4 px-6 rounded-full w-full transition-all duration-300"
+                            >
+                                New Category
+                            </button>
+                            <button
+                                onClick={onQuit}
+                                className="shadow-category-btn hover:shadow-category-btn-hover bg-[#FE71FE] hover:bg-[#7199FF] text-white py-4 px-6 rounded-full w-full transition-all duration-300"
+                            >
+                                Quit Game
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     );
 };
-
-export default GameModal;
